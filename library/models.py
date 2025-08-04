f867aa494f967a240a639c5a745639c7032d1c78
admin <admin@admin.com>
2025-08-04 17:54:34 +0200

    Create view for Book model

--- library/models.py
+++ library/models.py
@@ -22,7 +22,7 @@ class User(AbstractUser):
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
-    expected_return_date = models.DateField(null=True, blank=True)
+    expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
 
     book = models.ForeignKey("Book", on_delete=models.CASCADE)
