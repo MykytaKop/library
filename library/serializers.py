bdfbef590ecc02732ab8a5fbd25338d4ecd36f3b
admin <admin@admin.com>
2025-08-05 23:46:44 +0200

    Create serializers for borrowing model

--- library/serializers.py
+++ library/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from library.models import Book
+from user.serializers import UserSerializer
+from .models import Book, Borrowing
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -8,3 +9,37 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ["id", "title", "author", "cover", "inventory"]
 
+
+class BrowningListSerializer(serializers.ModelSerializer):
+    book = BookSerializer()
+    user = UserSerializer()
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        ]
+
+
+class BorrowingSerializerPost(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "expected_return_date",
+            "book",
+        ]
+
+
+class BorrowingSerializerUpdate(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "actual_return_date",
+        ]
