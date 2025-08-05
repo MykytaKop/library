f2b1282bf227da5aa3ac039383f28622eeec11e3
admin <admin@admin.com>
2025-08-05 23:48:14 +0200

    Create endpoints for borrowing view

--- library/urls.py
+++ library/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from library.views import BookListView
+from .views import (
+    BookListView,
+    BorrowingListView,
+)
 
 app_name = "library"
 router = routers.DefaultRouter()
 
 router.register("books", BookListView)
+router.register("borrowings", BorrowingListView)
 
 urlpatterns = [
     path("", include(router.urls)),
